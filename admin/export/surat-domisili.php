64bcd588a3371ba48075f4eaa390138c05027fe1
Frengky12 <frengkysky645@gmail.com>
2024-06-20 11:59:38 +0700

    feat: add export letters

---
+++ admin/export/surat-domisili.php
@@ -0,0 +1,79 @@
+<?php
+require('../vendor/setasign/fpdf/fpdf.php');
+
+class PDF extends FPDF
+{
+    // Page header
+    function Header()
+    {
+        // Logo
+        $this->Image('../public/image/img/Pemkab.png',10,5,25); // Ganti 'path/to/logo.png' dengan path logo Anda
+        $this->SetFont('Arial','B',12);
+        $this->Cell(0,7,'PEMERINTAH KABUPATEN MUSI BANYUASIN',0,1,'C');
+        $this->Cell(0,7,'KECAMATAN BABAT TOMAN',0,1,'C');
+        $this->Cell(0,7,'DESA KASMARAN',0,1,'C');
+        $this->SetFont('Arial','',10);
+        $this->Cell(0,7,'Alamat: Jalan Propinsi Sekayu - Mangunjaya Desa Kasmaran Kecamatan Babat Toman 30772',0,1,'C');
+        // $this->Ln(3);
+    }
+
+    // Page footer
+    // function Footer()
+    // {
+    //     $this->SetY(-15);
+    //     $this->SetFont('Arial','I',8);
+    //     $this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
+    // }
+}
+
+// Instanciation of inherited class
+$pdf = new PDF();
+$pdf->AliasNbPages();
+$pdf->AddPage();
+$pdf->SetFont('Arial','B',12);
+$pdf->Cell(0,10,'SURAT KETERANGAN BERDOMISILI',0,1,'C');
+$pdf->SetFont('Arial','',10);
+$pdf->Cell(0,3,'NOMOR : 503 / 2009/IV/2023',0,1,'C');
+
+// Add a line under the title
+$pdf->Line(10, 37, 200, 37);
+$pdf->Ln(10);
+
+$pdf->SetFont('Arial','',12);
+$pdf->MultiCell(0,5,'Yang bertanda tangan dibawah ini, Kepala Desa Kasmaran Kecamatan Babat Toman Kabupaten Musi Banyuasin Provinsi Sumatera Selatan, dengan ini menerangkan bahwa :',0,'L');
+$pdf->Ln(5);
+
+$data = [
+    'Nama' => 'YUNIAR EMINANSI',
+    'No NIK' => '1606065806910002',
+    'Jenis Kelamin' => 'Perempuan',
+    'Tempat/Tanggal Lahir' => 'Padang Sidempuan, 18-06-1991',
+    'Agama / Bangsa' => 'Islam / Indonesia',
+    'Status Perkawinan' => 'Kawin',
+    'Pekerjaan' => 'Apoteker',
+    'Alamat' => 'Jl. Tanjung Sari 2 No. 2 RT 033 RW 007 Kel. Bukit Sangkal Kec. Kalidoni Kota Palembang'
+];
+
+foreach ($data as $key => $value) {
+    $pdf->Cell(60,7,$key,0,0,'L');
+    $pdf->Cell(5,7,':',0,0,'L');
+    $pdf->MultiCell(0,7,$value,0,'L');
+}
+
+$pdf->Ln(7);
+$pdf->MultiCell(0,7,'Nama tersebut diatas benar menetap / Berdomisili di Dusun II Desa Kasmaran Kecamatan Babat Toman Kabupaten Musi Banyuasin Provinsi Sumatera Selatan sampai dengan sekarang.',0,'L');
+$pdf->Ln(7);
+$pdf->MultiCell(0,7,'Demikianlah Surat Keterangan ini kami buat dengan sebenaranya untuk dapat dipergunakan sebagaimana mestinya.',0,'L');
+$pdf->Ln(10);
+
+$pdf->SetX(120);
+$pdf->Cell(0,5,'Kasmaran, 21 Februari 2024',0,1,'C');
+// Position the signature to the right
+$pdf->SetX(120);
+$pdf->Cell(0,10,'KEPALA DESA KASMARAN',0,1,'C');
+$pdf->Ln(20);
+$pdf->SetX(120);
+$pdf->Cell(0,10,'FAHRUL ROZI, S.Pd',0,1,'C');
+
+$pdf->Output();
+?>
---
+++ admin/export/surat-kematian.php
@@ -0,0 +1,113 @@
+<?php
+require('../vendor/setasign/fpdf/fpdf.php');
+
+class PDF extends FPDF
+{
+    // Page header
+    function Header()
+    {
+        // Logo
+        $this->Image('../public/image/img/Pemkab.png',10,5,25); // Ganti 'path/to/logo.png' dengan path logo Anda
+        $this->SetFont('Arial','B',12);
+        $this->Cell(0,7,'PEMERINTAH KABUPATEN MUSI BANYUASIN',0,1,'C');
+        $this->Cell(0,7,'KECAMATAN BABAT TOMAN',0,1,'C');
+        $this->Cell(0,7,'DESA KASMARAN',0,1,'C');
+        $this->SetFont('Arial','',10);
+        $this->Cell(0,7,'Alamat: Jalan Propinsi Sekayu - Mangunjaya Desa Kasmaran Kecamatan Babat Toman 30772',0,1,'C');
+        // $this->Ln(3);
+    }
+
+    // Page footer
+    // function Footer()
+    // {
+    //     $this->SetY(-15);
+    //     $this->SetFont('Arial','I',8);
+    //     $this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
+    // }
+}
+
+// Instanciation of inherited class
+$pdf = new PDF();
+$pdf->AliasNbPages();
+$pdf->AddPage();
+$pdf->SetFont('Arial','B',12);
+$pdf->Cell(0,10,'SURAT KETERANGAN KEMATIAN',0,1,'C');
+$pdf->SetFont('Arial','',10);
+$pdf->Cell(0,3,'NOMOR : 503 / 2009/IV/2023',0,1,'C');
+
+// Add a line under the title
+$pdf->Line(10, 37, 200, 37);
+$pdf->Ln(10);
+
+$pdf->SetFont('Arial','',12);
+$pdf->MultiCell(0,5,'Yang bertanda tangan dibawah ini, Kepala Desa Kasmaran Kecamatan Babat Toman Kabupaten Musi Banyuasin menerangkan bahwa :',0,'L');
+$pdf->Ln(5);
+
+$data = [
+    'Nama' => 'RUSNAWATI',
+    'NIK' => '1606065201540002',
+    'Jenis Kelamin' => 'Perempuan',
+    'Tempat Tgl. Lahir' => 'Kasmaran, 12-10-1965',
+    'Agama' => 'Islam',
+    'Pekerjaan' => 'Pegawai Negeri Sipil (PNS)',
+    'Alamat' => 'Dusun II Desa Kasmaran Kec. Babat Toman'
+];
+
+foreach ($data as $key => $value) {
+    $pdf->Cell(60,7,$key,0,0,'L');
+    $pdf->Cell(5,7,':',0,0,'L');
+    $pdf->MultiCell(0,7,$value,0,'L');
+}
+
+$pdf->Ln(5);
+$pdf->MultiCell(0,7,'Menerangkan dengan sesungguhnya bahwa benar nama tersebut diatas meninggal dunia pada :',0,'L');
+
+$death_info = [
+    'Hari' => 'Jumat',
+    'Tanggal' => '12 Januari 2024',
+    'Jam' => '15.35 WIB',
+    'Tempat' => 'Dirumah',
+    'Disebabkan karena' => 'Sakit'
+];
+
+foreach ($death_info as $key => $value) {
+    $pdf->Cell(60,7,$key,0,0,'L');
+    $pdf->Cell(5,7,':',0,0,'L');
+    $pdf->MultiCell(0,7,$value,0,'L');
+}
+
+$pdf->Ln(5);
+$pdf->MultiCell(0,5,'Demikianlah Surat Keterangan ini dibuat dengan sebenaranya untuk dapat dipergunakan sebagaimana mestinya.',0,'L');
+$pdf->Ln(7);
+
+$pdf->SetFont('Arial','B',12);
+$pdf->Cell(0,10,'SAKSI-SAKSI',0,1,'L');
+$pdf->SetFont('Arial','',12);
+
+$witnesses = [
+    'ROSYAF SHIDIQ, S.E. (Sekdes)' => '.................................',
+    'AJIE FAHMI FADLI, S.Pd. (Anak Kandung)' => '.................................'
+];
+
+$i = 1;
+foreach ($witnesses as $witness => $dots) {
+    $pdf->Cell(10,7,$i.'.',0,0,'L');
+    $pdf->Cell(85,7,$witness,0,0,'L');
+    $pdf->Cell(5,7,':',0,0,'L');
+    $pdf->Cell(0,7,$dots,0,1,'L');
+    $i++;
+}
+$pdf->Ln(10);
+
+
+$pdf->SetX(120);
+$pdf->Cell(0,5,'Kasmaran, 21 Februari 2024',0,1,'C');
+// Position the signature to the right
+$pdf->SetX(120);
+$pdf->Cell(0,10,'KEPALA DESA KASMARAN',0,1,'C');
+$pdf->Ln(20);
+$pdf->SetX(120);
+$pdf->Cell(0,10,'FAHRUL ROZI, S.Pd',0,1,'C');
+
+$pdf->Output();
+?>
