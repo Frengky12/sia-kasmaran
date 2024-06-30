266639c80557bf06af1a32a6a7602dbdf0550628
Frengky12 <frengkysky645@gmail.com>
2024-06-30 15:44:29 +0700

    fix: change variable .env

--- admin/sendEmailSurat.php
+++ admin/sendEmailSurat.php
@@ -51,7 +51,7 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
         $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'siakasmaran@gmail.com';                     //SMTP username
+        $mail->Username   = $_ENV['USENAME_EMAIL'];                     //SMTP username
         $mail->Password   = $_ENV['MAIL_PASSWORD_UNIQUE'];                               //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
         $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
